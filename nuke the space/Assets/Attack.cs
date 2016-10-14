1cfc06da45147bbd2d59b9bf924f8751270aea7c
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-10-14 17:17:13 +0800

    Demo And Attack Class
    attack detect demo
    attack class

---
+++ nuke the space/Assets/Attack.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+    public int damage;
+}
---
+++ nuke the space/Assets/DamageTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageTest : MonoBehaviour
+{
+    public int damage = 0;
+    public float damageDelay = 0.1f;
+    private int damageCurrent = 0;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("TryDamage", 0, damageDelay);
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        var attack = c.gameObject.transform.root.GetComponent<Attack>();
+        if (attack != null)
+            damageCurrent = attack.damage;
+        //Debug.Log(c);
+        Debug.Log(attack);
+        // damageing = true;
+    }
+
+    void TryDamage()
+    {
+        // if(damageing)
+        damage += damageCurrent;
+        // damageing = false;
+        damageCurrent = 0;
+    }
+}
