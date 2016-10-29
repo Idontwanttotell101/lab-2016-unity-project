4b413c677f83363cb58ceccfa7bb43065a5ec9ed
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-10-29 22:38:16 +0800

    develop new beam model

---
+++ nuke the space/Assets/New Attack Model/HP.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HP : MonoBehaviour
+{
+    [System.Serializable]
+    public class HPChangeEvent : UnityEngine.Events.UnityEvent<HP>
+    {
+        /*parameter : this*/
+    };
+
+    //[SerializeField]
+    [System.NonSerialized]
+    private float Max = float.PositiveInfinity;
+    [SerializeField]
+    private float Initial=1;
+    public HPChangeEvent BeforeHpChange;
+    public HPChangeEvent AfterHpChange;
+
+    private float _value;
+    public float Value
+    {
+        get
+        {
+            return _value;
+        }
+        private set
+        {
+            BeforeHpChange.Invoke(this);
+            _value = value;
+            CheckedHP();
+            AfterHpChange.Invoke(this);
+        }
+    }
+
+    private void CheckedHP()
+    {
+        if (Value < 0)
+            Destroy(this.gameObject);
+        if (Value > Max)
+            Value = Max;
+    }
+
+    void Start()
+    {
+        // do not trigger event because *Initial* is supposed to be set to certain value (not a change)
+        _value = Initial;
+    }
+}
---
+++ nuke the space/Assets/New Attack Model/LineAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineAttack : MonoBehaviour {
+    public Ray attack;
+    public float DamagePerPeriod=1;
+    public float DamagePeriod=1;
+    public float FirstDamageDelay=0;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
