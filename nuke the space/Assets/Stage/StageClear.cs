e1f7c7f70c858a52226806d42932f4c45cb3f08f
Yu Hsuan Hung <deanwinchester0326@gmail.com>
2016-10-31 19:28:26 +0800

    Add Scene Loader
    Stage 1 completed

---
+++ nuke the space/Assets/Stage/StageClear.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class StageClear : MonoBehaviour {
+    public GameObject ui;
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            ui.SetActive(true);
+        }
+    }
+}
