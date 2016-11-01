cb7ea73b95ed63489d9dce183d2f9c456dd982e5
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-11-01 19:09:52 +0800

    develop trying

---
+++ nuke the space/Assets/route/SingleRouteRenderer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleRouteRenderer : MonoBehaviour {
+    public Router Origin;
+    public Router Target;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
