bccb67fcaf46e37f6e45b83dcdc27a8cc1f5b716
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-11-02 11:11:06 +0800

    Single Route Renderer

--- nuke the space/Assets/route/SingleRouteRenderer.cs
+++ nuke the space/Assets/route/SingleRouteRenderer.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SingleRouteRenderer : MonoBehaviour {
+public class SingleRouteRenderer : MonoBehaviour
+{
     public Router Origin;
     public Router Target;
+    public bool IsPortal = false;
 
-	// Use this for initialization
-	void Start () {
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private static Material mat;
+    private static Material hmat;
+    private static bool MaterialLoaded;
+    public static Material GetRouteMaterial (bool isPortal){
+        if (!MaterialLoaded)
+        {
+            mat = Resources.Load<Material>("Route Material");
+            hmat = Resources.Load<Material>("Hint Route Material");
+        }
+        return isPortal ? hmat : mat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var diff = Target.transform.position - Origin.transform.position;
+        var distance = diff.magnitude;
+        this.transform.position = Origin.transform.position + diff / 2;
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.right, diff);
+        this.transform.localScale = new Vector3(distance / 2, this.transform.localScale.y, this.transform.localScale.z);
+        this.GetComponentInChildren<Renderer>().material = GetRouteMaterial(IsPortal);
+    }
 }
