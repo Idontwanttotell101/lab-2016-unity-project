f3a7eb544025f327b243f2e0df7e89543235f18b
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-11-08 17:47:48 +0800

    break

--- nuke the space/Assets/RouteEditor.cs
+++ nuke the space/Assets/RouteEditor.cs
@@ -1,5 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 
-public class RouteEditor : Editor {}
+[ExecuteInEditMode]
+public class RouteEditor : MonoBehaviour
+{
+    public List<Router> Routers;
+    public List<Transform> Transforms;
+
+    void Update() {
+        Routers =
+            GetComponentsInChildren<Router>()
+            .ToList();
+        Transforms =
+            Routers.Select(x => x.transform).ToList();
+    }
+}
--- nuke the space/Assets/route/Editor/RouteEditorScript.cs
+++ nuke the space/Assets/route/Editor/RouteEditorScript.cs
@@ -1,8 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using UnityEditorInternal;
 
 [CustomEditor(typeof(RouteEditor))]
-public class RouteEditorScript : Editor {
+public class RouteEditorScript : Editor
+{
+    private ReorderableList list;
 
+    private void OnEnable()
+    {
+        list = new ReorderableList(serializedObject,
+                serializedObject.FindProperty("Transforms"),
+                true, true, true, true);
+
+        list.drawElementCallback =
+    (Rect rect, int index, bool isActive, bool isFocused) =>
+    {
+        var element = list.serializedProperty.GetArrayElementAtIndex(index);
+        var pos = element.FindPropertyRelative("position");
+        rect.y += 2;
+
+        EditorGUILayout.PropertyField(pos, GUIContent.none);
+    };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        list.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void OnSceneGUI()
+    {
+        // get the chosen game object
+        RouteEditor t = target as RouteEditor;
+
+        if (t == null)
+            return;
+
+        // iterate over game objects added to the array...
+        //for (int i = 0; i < t.Routers.Count; i++)
+        // {
+        //    // ... and draw a line between them
+        //     if (t.[i] != null)
+        //        Handles.DrawLine(center, t.GameObjects[i].transform.position);
+        // }
+    }
 }
